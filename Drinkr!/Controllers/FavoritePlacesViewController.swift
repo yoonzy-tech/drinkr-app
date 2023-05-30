c6a14cdc8e5f1a3538d07d9682277690c83d62b5
yoonzy-tech <dev.rubyc@gmail.com>
2023-05-30 19:44:50 +0800

    add favortie place tableview

---
+++ Drinkr!/Controllers/FavoritePlacesViewController.swift
@@ -0,0 +1,49 @@
+//
+//  FavoritePlacesViewController.swift
+//  Drinkr!
+//
+//  Created by Ruby Chew on 2023/5/30.
+//
+
+import UIKit
+
+class FavoritePlacesViewController: UIViewController {
+
+    var dataSource: [Place] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+//extension FavoritePlacesViewController: UITableViewDataSource, UITableViewDelegate {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        dataSource.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(
+//            withIdentifier: "Cell", for: indexPath) as? Cell
+//        else { fatalError("Unable to generate Table View Cell") }
+        
+//        return cell
+//    }
+//}
+
